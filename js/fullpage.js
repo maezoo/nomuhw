8f46578911bd0100d2366ebc67fab1bd48e6ea9b
maezoo <aa00456@naver.com>
2026-03-13 18:08:33 +0900

    fullpage.js 세팅완료

---
+++ js/fullpage.js
@@ -0,0 +1,13 @@
+var myFullpage = new fullpage('#fullpage', {
+    verticalCentered: true,
+    anchors: ['anchor1', 'anchor2', 'anchor3'],
+    menu: '#menu',
+});
+
+new fullpage('#fullpage', {
+    // 옵션 설정
+    sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke'],
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+    menu: '#menu',
+    scrollingSpeed: 1000
+});
