7cb5e840806937bf76bf0b6bc6607a4582769761
maezoo <aa00456@naver.com>
2026-03-15 02:22:55 +0900

    mainpage section2: 레이아웃 정렬 및 콘텐츠 적용 작업

--- js/fullpage.js
+++ js/fullpage.js
@@ -6,8 +6,11 @@
 
 new fullpage('#fullpage', {
     // 옵션 설정
-    sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke'],
+    // sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke'],
     anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
     menu: '#menu',
-    scrollingSpeed: 1000
+    scrollingSpeed: 1000,
+    verticalCentered: false,
+    scrollOverflow: false,
+    
 });
